d2c3a39581436acd877539cea6f7b712300f8fca
Jamal Rogers <jamala@gmail.com>
2018-09-17 15:36:08 -0400

    Add glass material

--- cpp/tracer.cpp
+++ cpp/tracer.cpp
@@ -27,12 +27,13 @@ int main() {
 	int nx = 200;
 	int ny = 100;
 	int ns = 100;
-	hitable *list[4];
-	list[0] = new sphere(vec3(0.0, 0.0, -1.0), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+	hitable *list[5];
+	list[0] = new sphere(vec3(0.0, 0.0, -1.0), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
 	list[1] = new sphere(vec3(0.0, -100.5, -1.0), 100.0, new lambertian(vec3(0.8, 0.8, 0.0)));
 	list[2] = new sphere(vec3(1.0, 0.0, -1.0), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-	list[3] = new sphere(vec3(-1.0, 0.0, -1.0), 0.5, new metal(vec3(0.8, 0.8, 0.8), 1.0));
-	hitable *world = new hitable_list(list, 4);
+	list[3] = new sphere(vec3(-1.0, 0.0, -1.0), 0.5, new dielectric(1.5));
+	list[4] = new sphere(vec3(-1.0, 0.0, -1.0), -0.45, new dielectric(1.5));
+	hitable *world = new hitable_list(list, 5);
 	camera cam;
 	
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
