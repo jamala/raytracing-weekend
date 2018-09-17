17b8f9871093c31117c6ac41eb8dc0340a9edfee
Jamal Rogers <jamala@gmail.com>
2018-09-17 13:54:44 -0400

    Add metal material

--- cpp/sphere.h
+++ cpp/sphere.h
@@ -6,10 +6,11 @@
 class sphere: public hitable {
 public:
 	sphere() {}
-	sphere(vec3 cen, double r) : center(cen), radius(r) {}
+	sphere(vec3 cen, double r, material *m) : center(cen), radius(r), mat_ptr(m) {}
 	virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const;
 	vec3 center;
 	double radius;
+	material * mat_ptr;
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
@@ -24,6 +25,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 			rec.t = temp;
 			rec.p = r.point_at_parameter(rec.t);
 			rec.normal = (rec.p - center) / radius;
+			rec.mat_ptr = mat_ptr;
 			return true;
 		}
 		temp = (-b + sqrt(b*b - a*c))/a;
@@ -31,6 +33,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 			rec.t = temp;
 			rec.p = r.point_at_parameter(rec.t);
 			rec.normal = (rec.p - center) / radius;
+			rec.mat_ptr = mat_ptr;
 			return true;
 		}
 	}
