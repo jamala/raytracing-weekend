99e2cc79a70c5258ebccbe06ea40c8f9fa10c46a
Jamal Rogers <jamala@gmail.com>
2018-09-17 00:09:26 -0400

    Extract camera code and add diffuse color

--- cpp/tracer.cpp
+++ cpp/tracer.cpp
@@ -2,11 +2,21 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
+
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1.0, 1.0, 1.0);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
 
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, DBL_MAX, rec)) {
-		return 0.5*vec3(rec.normal.x() + 1.0, rec.normal.y() + 1.0, rec.normal.z() + 1.0);
+	if (world->hit(r, 0.001, DBL_MAX, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5*color(ray(rec.p, target - rec.p), world);
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -18,25 +28,26 @@ vec3 color(const ray& r, hitable *world) {
 int main() {
 	int nx = 200;
 	int ny = 100;
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
+	int ns = 100;
 	hitable *list[2];
 	list[0] = new sphere(vec3(0.0, 0.0, -1.0), 0.5);
 	list[1] = new sphere(vec3(0.0, -100.5, -1.0), 100.0);
 	hitable *world = new hitable_list(list, 2);
-
+	camera cam;
 	
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny-1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			double u = double(i) / double(nx);
-			double v = double(j) / double(ny);
-			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-
-			vec3 p = r.point_at_parameter(2.0);
-			vec3 col = color(r, world);
+			vec3 col(0.0, 0.0, 0.0);
+			for (int s = 0; s < ns; s++){
+				double u = double(i + drand48()) / double(nx);
+				double v = double(j + drand48()) / double(ny);
+				ray r = cam.get_ray(u, v);
+				vec3 p = r.point_at_parameter(2.0);
+				col += color(r, world);
+			}
+			col /= double(ns);
+			col = vec3 (sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
